b249029b59fd7b7ff67dcb135fd5645587bcb336
hansraj <hansraj.bcsf18@iba-suk.edu.pk>
2023-03-25 10:11:14 +0500

    done profile

--- lib/dashboard/profile.dart
+++ lib/dashboard/profile.dart
@@ -22,6 +22,9 @@ class _ProfileState extends State<Profile> {
     "assets/images/img_thor.png",
     "assets/images/img_hotel.png",
     "assets/images/img_hostile.png",
+    "assets/images/img_thor.png",
+    "assets/images/img_hotel.png",
+    "assets/images/img_hostile.png",
   ];
 
   @override
@@ -30,188 +33,161 @@ class _ProfileState extends State<Profile> {
     return Scaffold(
       backgroundColor: Colors.black,
       body: SafeArea(
-        child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: [
-              Padding(
-                padding: const EdgeInsets.only(top: 30.0, left: 15),
-                child: Row(children: [
-                  Expanded(
-                    child: VariableText(
-                      text: "Profile",
-                      fontcolor: primaryColorW,
-                      fontsize: size.height * 0.02,
-                      fontFamily: fontMedium,
-                      weight: FontWeight.w500,
-                    ),
-                  ),
-                  IconButton(
-                      icon: Icon(Icons.settings, color: Colors.white),
-                      onPressed: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) => const Setting()),
-                        );
-                      }),
-                ]),
-              ),
-              SizedBox(
-                child: Container(
-                  color: Colors.black,
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      CircleAvatar(
-                        radius: 60,
-                        backgroundImage: AssetImage("assets/images/Avatar.png"),
-                        backgroundColor: Colors.black,
-                      ),
-                      VariableText(
-                        text: "Muhammad Bilal",
-                        fontsize: size.height * 0.024,
+        child: SingleChildScrollView(
+          child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(top: 30.0, left: 15),
+                  child: Row(children: [
+                    Expanded(
+                      child: VariableText(
+                        text: "Profile",
+                        fontcolor: primaryColorW,
+                        fontsize: size.height * 0.02,
                         fontFamily: fontMedium,
                         weight: FontWeight.w500,
-                        max_lines: 2,
                       ),
-                      SizedBox(
-                        height: 15,
-                      ),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceAround,
-                        children: [
-                          Container(
-                            color: textColorS,
-                            child: Column(
-                              children: [
-                                VariableText(
-                                  text: "3120",
-                                  fontcolor: textColor5,
-                                  fontsize: size.height * 0.016,
-                                  fontFamily: fontMedium,
-                                  weight: FontWeight.w500,
-                                  max_lines: 2,
-                                ),
-                                SizedBox(height: 10),
-                                Column(
-                                  children: [
-                                    VariableText(
-                                      text: "Likes",
-                                      fontcolor: textColor5,
-                                      fontsize: size.height * 0.016,
-                                      fontFamily: fontMedium,
-                                      weight: FontWeight.w500,
-                                      max_lines: 2,
-                                    ),
-                                  ],
-                                ),
-                              ],
-                            ),
-                          ),
-                          SizedBox(
-                            width: size.width * 0.02,
-                          ),
-                          Container(
-                            color: textColorS,
-                            child: Column(
-                              children: [
-                                VariableText(
-                                  text: "1232",
-                                  fontcolor: textColor5,
-                                  fontsize: size.height * 0.016,
-                                  fontFamily: fontMedium,
-                                  weight: FontWeight.w500,
-                                  max_lines: 2,
-                                ),
-                                SizedBox(height: 10),
-                                Column(
-                                  children: [
-                                    VariableText(
-                                      text: "Watching",
-                                      fontcolor: textColor5,
-                                      fontsize: size.height * 0.016,
-                                      fontFamily: fontMedium,
-                                      weight: FontWeight.w500,
-                                      max_lines: 2,
-                                    ),
-                                  ],
-                                ),
-                              ],
-                            ),
-                          ),
-                          SizedBox(
-                            width: size.width * 0.02,
-                          ),
-                          Container(
-                            color: textColorS,
-                            child: Column(
-                              children: [
-                                VariableText(
-                                  text: "44",
-                                  fontsize: size.height * 0.016,
-                                  fontcolor: textColor5,
-                                  fontFamily: fontMedium,
-                                  weight: FontWeight.w500,
-                                  max_lines: 2,
-                                ),
-                                SizedBox(height: 10),
-                                Column(
-                                  children: [
-                                    VariableText(
-                                      text: "Subscribes",
-                                      fontcolor: textColor5,
-                                      fontsize: size.height * 0.016,
-                                      fontFamily: fontMedium,
-                                      weight: FontWeight.w500,
-                                      max_lines: 2,
-                                    ),
-                                  ],
-                                ),
-                              ],
-                            ),
-                          )
-                        ],
-                      ),
-                      SizedBox(
-                        height: 10,
-                      ),
-                      Container(
-                        width: size.width,
-                        height: size.height * 0.6,
-                        padding: EdgeInsets.symmetric(
-                            vertical: size.height * verticalPadding),
-                        child: Row(
+                    ),
+                    IconButton(
+                        icon: Icon(Icons.settings, color: Colors.white),
+                        onPressed: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => const Setting()),
+                          );
+                        }),
+                  ]),
+                ),
+                SizedBox(
+                  child: Container(
+                    color: Colors.black,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        CircleAvatar(
+                          radius: 60,
+                          backgroundImage:
+                              AssetImage("assets/images/Avatar.png"),
+                          backgroundColor: Colors.black,
+                        ),
+                        VariableText(
+                          text: "Muhammad Bilal",
+                          fontsize: size.height * 0.024,
+                          fontFamily: fontMedium,
+                          weight: FontWeight.w500,
+                          max_lines: 2,
+                        ),
+                        SizedBox(
+                          height: 15,
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                           children: [
-                            Expanded(
-                              child: GridView.builder(
-                                  itemCount: images.length,
-                                  gridDelegate:
-                                      SliverGridDelegateWithFixedCrossAxisCount(
-                                    crossAxisCount: 2,
-                                    // crossAxisSpacing: size.width * 0.03,
-                                    // mainAxisSpacing: size.height * 0.015,
-                                    //childAspectRatio: 0.63,
-                                    childAspectRatio:
-                                        size.width / (size.height * 0.36),
-                                  ),
-                                  shrinkWrap: false,
-                                  scrollDirection: Axis.horizontal,
-                                  physics: ScrollPhysics(),
-                                  itemBuilder: (_, index) {
-                                    return Container(
-                                        child: Image.asset(images[index]));
-                                  }),
+                            CustomProfileBox(
+                              title: "3120",
+                              subtitle: "Likes",
+                            ),
+                            CustomProfileBox(
+                              title: "1232",
+                              subtitle: "Watching",
+                            ),
+                            InkWell(
+                              onTap: () {
+                                //Todo
+                              },
+                              child: CustomProfileBox(
+                                title: "44",
+                                subtitle: "Subscribes",
+                              ),
                             ),
                           ],
                         ),
-                      ),
-                    ],
+                        SizedBox(
+                          height: 10,
+                        ),
+                        Container(
+                          width: size.width,
+                          height: size.height * 0.6,
+                          padding: EdgeInsets.symmetric(
+                              vertical: size.height * verticalPadding),
+                          child: Column(
+                            children: [
+                              Expanded(
+                                child: GridView.builder(
+                                    itemCount: images.length,
+                                    gridDelegate:
+                                        SliverGridDelegateWithFixedCrossAxisCount(
+                                      crossAxisCount: 3,
+                                      // crossAxisSpacing: size.width * 0.03,
+                                      // mainAxisSpacing: size.height * 0.015,
+                                      //childAspectRatio: 0.63,
+                                      childAspectRatio:
+                                          size.width / (size.height * 0.9),
+                                    ),
+                                    shrinkWrap: false,
+                                    scrollDirection: Axis.vertical,
+                                    physics: ScrollPhysics(),
+                                    itemBuilder: (_, index) {
+                                      return Container(
+                                          child: Image.asset(images[index]));
+                                    }),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
                   ),
                 ),
+              ]),
+        ),
+      ),
+    );
+  }
+}
+
+class CustomProfileBox extends StatelessWidget {
+  String? title;
+  String? subtitle;
+  CustomProfileBox({super.key, this.title, this.subtitle});
+
+  @override
+  Widget build(BuildContext context) {
+    var size = MediaQuery.of(context).size;
+    return Container(
+      width: size.width * 0.23,
+      height: size.width * 0.22,
+      padding: EdgeInsets.all(10),
+      decoration: BoxDecoration(
+          color: textColorS, borderRadius: BorderRadius.circular(10)),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          VariableText(
+            text: title,
+            fontcolor: primaryColor1,
+            fontsize: size.height * 0.018,
+            fontFamily: fontMedium,
+            weight: FontWeight.w600,
+          ),
+          SizedBox(height: 10),
+          Column(
+            children: [
+              VariableText(
+                text: subtitle,
+                fontcolor: textColor5,
+                fontsize: size.height * 0.017,
+                fontFamily: fontMedium,
+                weight: FontWeight.w500,
+                max_lines: 2,
               ),
-            ]),
+            ],
+          ),
+        ],
       ),
     );
   }
--- lib/dashboard/setting.dart
+++ lib/dashboard/setting.dart
@@ -59,11 +59,29 @@ class _SettingState extends State<Setting> {
             SizedBox(
               height: 10,
             ),
-
-            buildAccountOption(context, 'Clear cache'),
-            buildAccountOption(context, "Share the app"),
-            buildAccountOption(context, 'Forget Password'),
-            buildAccountOption(context, 'About'),
+            ExpansionTile(
+              title: VariableText(
+                text: "Clear Cache",
+                fontcolor: primaryColorW,
+                fontsize: size.height * 0.022,
+                fontFamily: fontSemiBold,
+                textAlign: TextAlign.start,
+                weight: FontWeight.w500,
+              ),
+              trailing: VariableText(
+                text: "250MB",
+                fontcolor: primaryColorW,
+                fontsize: size.height * 0.022,
+                fontFamily: fontSemiBold,
+                textAlign: TextAlign.start,
+                weight: FontWeight.w500,
+              ),
+              iconColor: primaryColorW,
+            ),
+            // buildAccountOption(context, 'Clear cache'),
+            // buildAccountOption(context, "Share the app"),
+            // buildAccountOption(context, 'Forget Password'),
+            // buildAccountOption(context, 'About'),
             SizedBox(
               height: 60,
             ),
@@ -107,52 +125,53 @@ class _SettingState extends State<Setting> {
       ),
     );
   }
-
-  GestureDetector buildAccountOption(BuildContext context, String title) {
-    return GestureDetector(
-      onTap: () {
-        showAboutDialog(
-            context: context,
-            builder: (BuildContext context) {
-              return AlertDialog(
-                title: Text(""),
-                content: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    Text("Option 1"),
-                    Text("Option 1"),
-                    Text("Option 1"),
-                  ],
-                ),
-                actions: [
-                  TextButton(
-                    onPressed: () {
-                      Navigator.of(context).pop();
-                    },
-                    child: Text("Close"),
-                  )
-                ],
-              );
-            });
-      },
-      child: Padding(
-        padding: EdgeInsets.symmetric(vertical: 8, horizontal: 20),
-        child:
-            Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
-          Text(
-            title,
-            style: TextStyle(
-              fontSize: 20,
-              fontWeight: FontWeight.w500,
-              color: textColor5,
-            ),
-          ),
-          Icon(
-            Icons.arrow_forward_ios,
-            color: textColor5,
-          ),
-        ]),
-      ),
-    );
-  }
 }
+
+//   GestureDetector buildAccountOption(BuildContext context, String title) {
+//     return GestureDetector(
+//       onTap: () {
+//         showAboutDialog(
+//             context: context,
+//             builder: (BuildContext context) {
+//               return AlertDialog(
+//                 title: Text(""),
+//                 content: Column(
+//                   mainAxisSize: MainAxisSize.min,
+//                   children: [
+//                     Text("Option 1"),
+//                     Text("Option 1"),
+//                     Text("Option 1"),
+//                   ],
+//                 ),
+//                 actions: [
+//                   TextButton(
+//                     onPressed: () {
+//                       Navigator.of(context).pop();
+//                     },
+//                     child: Text("Close"),
+//                   )
+//                 ],
+//               );
+//             });
+//       },
+//       child: Padding(
+//         padding: EdgeInsets.symmetric(vertical: 8, horizontal: 20),
+//         child:
+//             Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
+//           Text(
+//             title,
+//             style: TextStyle(
+//               fontSize: 20,
+//               fontWeight: FontWeight.w500,
+//               color: textColor5,
+//             ),
+//           ),
+//           Icon(
+//             Icons.arrow_forward_ios,
+//             color: textColor5,
+//           ),
+//         ]),
+//       ),
+//     );
+//   }
+// }
