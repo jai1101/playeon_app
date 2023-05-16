801806650dd5becc5cb8f62530b7ad4366b8253c
jai <mjai15709@gmail.com>
2023-05-16 22:56:14 +0500

    video player

--- lib/dashboard/videoplayer.dart
+++ lib/dashboard/videoplayer.dart
@@ -1,78 +1,78 @@
-import 'package:flutter/material.dart';
-import 'package:video_player/video_player.dart';
+// import 'package:flutter/material.dart';
+// import 'package:video_player/video_player.dart';
 
-class VideoDemo extends StatefulWidget {
-  const VideoDemo({super.key});
+// class VideoDemo extends StatefulWidget {
+//   const VideoDemo({super.key});
 
-  final String title = "Video Demo";
+//   final String title = "Video Demo";
 
-  @override
-  State<VideoDemo> createState() => _VideoDemoState();
-}
+//   @override
+//   State<VideoDemo> createState() => _VideoDemoState();
+// }
 
-class _VideoDemoState extends State<VideoDemo> {
-  late VideoPlayerController _controller;
-  late Future <void> _initializeVideoPlayerFuture;
+// class _VideoDemoState extends State<VideoDemo> {
+//   late VideoPlayerController _controller;
+//   late Future <void> _initializeVideoPlayerFuture;
 
 
-  @override
-  void initState(){
-    _controller = VideoPlayerController.network(
-       "https://flutter.github.io/assets-for-api-docs/assets/videos/butterfly.mp4",
+//   @override
+//   void initState(){
+//     _controller = VideoPlayerController.network(
+//        "https://flutter.github.io/assets-for-api-docs/assets/videos/butterfly.mp4",
        
-        _initializeVideoPlayerFuture = _controller.initialize();
+//         _initializeVideoPlayerFuture = _controller.initialize();
 
        
 
-        // _controller.setLooping(true);
-        _controller.setVolume(1.0);
+//         // _controller.setLooping(true);
+//         _controller.setVolume(1.0);
 
 
-        super.initState();
+//         super.initState();
 
-    );
+//     );
     
   
-  }
-   @override
-   void dispose(){
-    _controller.dispose();
-    super.dispose();
-   }
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: FutureBuilder(
-    future: _initializeVideoPlayerFuture,
-    builder: (context , snapshot){
-      if(snapshot.connectionState == ConnectionState.done){
-        return AspectRatio(
-    aspectRatio: _controller.value.aspectRatio,
-    child: VideoPlayer(_controller),
+//   }
+//    @override
+//    void dispose(){
+//     _controller.dispose();
+//     super.dispose();
+//    }
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       body: FutureBuilder(
+//     future: _initializeVideoPlayerFuture,
+//     builder: (context , snapshot){
+//       if(snapshot.connectionState == ConnectionState.done){
+//         return AspectRatio(
+//     aspectRatio: _controller.value.aspectRatio,
+//     child: VideoPlayer(_controller),
 
-        );
-      }
-      else {
-        return Center(
-          child: CircularProgressIndicator(),
-        );
-      }
-    },
-      ),
-    floatingActionButton : FloatingActionButton(
-      onPressed: () {
-        setState(() {
-          if(_controller.value.isPlaying){
-            _controller.pause();
-          }
-          else{
-            _controller.play();
+//         );
+//       }
+//       else {
+//         return Center(
+//           child: CircularProgressIndicator(),
+//         );
+//       }
+//     },
+//       ),
+//     floatingActionButton : FloatingActionButton(
+//       onPressed: () {
+//         setState(() {
+//           if(_controller.value.isPlaying){
+//             _controller.pause();
+//           }
+//           else{
+//             _controller.play();
         
-          }
-        }
-        );
-      },
-      child: Icon(_controller.value.isPlaying ? Icons.pause: Icons.play_arrow  ),)
+//           }
+//         }
+//         );
+//       },
+//       child: Icon(_controller.value.isPlaying ? Icons.pause: Icons.play_arrow  ),)
       
-    );
-  }
-}
+//     );
+//   }
+// }
